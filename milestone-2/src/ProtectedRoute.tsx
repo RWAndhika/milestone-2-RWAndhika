318856d337979e6c8193489b28520f249690aa67
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-04-29 20:49:32 +0700

    feat: first init

---
+++ milestone-2/src/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+// import { Routes, Route, Link, Navigate } from 'react-router-dom';
+
+// const ProtectedRoute = ({ user: any, children: any }) => {
+//     if (!user) {
+//         return <Navigate to="/home" replace />;
+//     }
+
+//     return children;
+// };
+
+// export default ProtectedRoute;
