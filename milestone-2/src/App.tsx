f6e3283027b6f1f99a03a76b2f10a7da35abbd64
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-04-30 19:21:13 +0700

    feat: add protected route

--- milestone-2/src/App.tsx
+++ milestone-2/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-// import ProtectedRoute from './ProtectedRoute';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,12 +9,16 @@ import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
 
 function App() {
+  const isAllowed = localStorage.getItem('token');
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<ProtectedRoute isAllowed={!!isAllowed as Boolean} />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
