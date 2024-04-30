cd082ac01bfc0684d5b6af9a88a8971d5e81f3f4
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-04-30 19:24:02 +0700

    feat: add form to login

--- milestone-2/src/pages/Login.tsx
+++ milestone-2/src/pages/Login.tsx
@@ -1,6 +1,77 @@
+import { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+
+    const [name, setName] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const initialValues = {
+        name: "",
+        password: ""
+    }
+
+    const navigate = useNavigate();
+
+    const loginSchema = Yup.object().shape({
+        name: Yup.string().required("Required"),
+
+        password: Yup.string().required("Required"),
+    });
+
+    const handleLogin = (name: string, password: string) => {
+        if (name === localStorage.name && password === localStorage.password) {
+            alert("Login successful");
+            navigate('/dashboard');
+        } else {
+            alert("Login failed");
+        }
+    }
+
     return (
-        <h1>Login</h1>
+        <section className="bg-yellow-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                    Pokedex
+                </a>
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Sign in to Pokedex
+                        </h1>
+                        <Formik
+                            initialValues={initialValues}
+                            validationSchema={loginSchema}
+                            onSubmit={(values) => {
+                                handleLogin(values.name, values.password)
+                            }}
+                        >
+                            <Form className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name: </label>
+                                    <Field type="text" id="name" name="name" placeholder="Enter your name"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                    <ErrorMessage name="name" component="div" />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password: </label>
+                                    <Field type="password" id="password" name="password" placeholder="••••••••"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                    <ErrorMessage name="password" component="div" />
+                                </div>
+                                <button type="submit" className="w-full text-white bg-blue-700 bg-primary-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Don’t have an account yet? <a onClick={() => navigate('/register')} className="cursor-pointer font-medium text-primary-600 hover:underline dark:text-primary-500">Register</a>
+                                </p>
+                            </Form>
+                        </Formik>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
